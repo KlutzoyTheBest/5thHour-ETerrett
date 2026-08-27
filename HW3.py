d04aabe37e52e746462c8ddb4fde40aabc454b8a
Ethan Terrett <et126336@Student.wpsok.org>
2026-08-27 12:43:48 -0500

    Turned in HW3

---
+++ HW3.py
@@ -0,0 +1,47 @@
+#Name: Ethan Terrett
+#Class: 5th Hour
+#Assignment: HW3
+import decimal
+
+#1. Print "Hello World!"
+
+print("hello world")
+
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+
+ALittleNumber = 3
+BobbyTalk = "I had a lot to eat today!"
+Goodbye = False
+
+#3. Print all three variables on the same print function (at the same time).
+
+print(ALittleNumber, BobbyTalk, Goodbye)
+
+#4. Create a variable that asks the user to input an integer.
+
+x = int(input("Please enter a number:"))
+
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+
+print(x + ALittleNumber)
+
+#6. Take the result from #5 and divide it by 2. Print the result.
+
+Sum = x + ALittleNumber
+print(Sum / 2)
+
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+
+Goodbye = True
+
+#8. Print the value of the boolean variable.
+
+print(Goodbye)
+
+#9. Create a variable with a number that contains decimals.
+
+print(4.222222222)
+
+#10. Round the number from #9 up or down using the round function.
+
+print(round(4.222222222, 4))
